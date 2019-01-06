c432d1afef78757f165e5ad16e5413b9bc11a31b
Tomasz Biernacki <tmbiernacki@szczecin.home.pl>
2019-01-06 10:33:41 +0100

    Modify topbar

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 'use strict';
 
+var inactiveMenuLink = document.querySelectorAll('.menu-list-item');
+var inactivePageView = document.querySelectorAll('.view-page');
+
+/* remove highlight menu link */
+function removeLink() {
+	for (var i = 0; i < inactiveMenuLink.length; i++) {
+		inactiveMenuLink[i].classList.remove('link-change-view');
+	}
+}
+
+/* hide page */
+function removePage() {
+	for (var i = 0; i < inactivePageView.length; i++) {
+		inactivePageView[i].classList.remove('page-active');
+		inactivePageView[i].classList.add('page-inactive');
+	}
+}
+
+/* change displayed page and highlight menu link */
 var changeView = function (idLink, idView) {
 	var activeMenuLink = document.getElementById(idLink);
 	var activePageView = document.getElementById(idView);
-	var inactiveMenuLink = document.querySelectorAll('.link-change-view');
-	var inactivePageView = document.querySelectorAll('.page-active');
 
-	function removeLink() {
-		for (var i = 0; i < inactiveMenuLink.length; i++) {
-			inactiveMenuLink[i].classList.remove('link-change-view');
-		}
-	}
 	removeLink();
-
-	function removePage() {
-		for (var i = 0; i < inactivePageView.length; i++) {
-			inactivePageView[i].classList.remove('page-active');
-			inactivePageView[i].classList.add('page-inactive');
-		}
-	}
 	removePage();
 
 	activeMenuLink.classList.add('link-change-view');
