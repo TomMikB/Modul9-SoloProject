1f0b3f996a1ac54bc3b45720da9cc1ee43e9061b
Tomasz Biernacki <tmbiernacki@szczecin.home.pl>
2019-02-06 04:54:21 +0100

    Add popups

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Modul9-SoloProject
 kodilla - modul 9: solo project
+https://tommikb.github.io/Modul9-SoloProject/
--- js/script.js
+++ js/script.js
@@ -93,3 +93,31 @@ document.getElementById("rangeInput").oninput = function() {
 	var hoursStyle = document.getElementById("hoursPosition").style;
 	hoursStyle.left = 'calc(' + this.value/168*100 + '% - ' + (this.value/168*60-20) + 'px)'
 }
+
+/* popups */
+function closeModal() {
+	document.getElementById('overlay').classList.remove('show-popup')
+}
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+	btn.addEventListener('click', function(e) {
+		e.preventDefault()
+		closeModal()
+	})
+})
+document.querySelector('#overlay').addEventListener('click', function(e) {
+	if(e.target === this) {
+		closeModal()
+	}
+})
+document.addEventListener('keyup', function(e) {
+	if(e.keyCode === 27) {
+		closeModal()
+	}
+})
+function openModal(modal) {
+	document.querySelectorAll('#overlay > *').forEach(function(modal) {
+		modal.classList.remove('show-popup')
+	})
+	document.querySelector('#overlay').classList.add('show-popup')
+	document.querySelector(modal).classList.add('show-popup')
+}
